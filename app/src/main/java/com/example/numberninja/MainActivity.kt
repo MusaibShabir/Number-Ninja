1451314b2bc92e35de967a75eb2136035bfe6044
Musaib Shabir <bnny633@gmail.com>
2024-01-31 01:05:00 +0530

    Splash Screen Added

--- app/src/main/java/com/example/numberninja/MainActivity.kt
+++ app/src/main/java/com/example/numberninja/MainActivity.kt
@@ -10,12 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.numberninja.ui.HomeScreen
 import com.example.numberninja.ui.theme.NumberNinjaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NumberNinjaTheme {
                 // A surface container using the 'background' color from the theme
